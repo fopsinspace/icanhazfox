fa811854be0083a66c616b710fda183063b6c5eb
Flero <flero@flero.dev>
2023-11-06 09:40:25 +1030

    fix(web): position of close button on image dialog

--- packages/web/src/components/Image.tsx
+++ packages/web/src/components/Image.tsx
@@ -39,15 +39,12 @@ export default function Image(props: ImageProps) {
         </Dialog.Trigger>
         <Dialog.Portal>
           <Dialog.Overlay className="backdrop-blur-md backdrop-brightness-90 fixed top-0 left-0 right-0 bottom-0 grid place-items-center overflow-y-auto p-24">
-            <Dialog.Content className="min-w-full bg-neutral-200 dark:bg-neutral-800 rounded-xl p-12">
+            <Dialog.Content className="min-w-full bg-neutral-200 dark:bg-neutral-800 rounded-xl p-12 relative">
               {/* TODO: Support for animated content */}
               {/* Also, don't mute the video. The user needs to have interacted to show this, so audio will work. */}
               <img src={props.src} className="w-1/3" loading="lazy" />
 
-              <Dialog.Close
-                aria-label="Close"
-                className="absolute top-28 right-28"
-              >
+              <Dialog.Close aria-label="Close" className="absolute top-4 right-4">
                 <IconX className="text-neutral-800 dark:text-neutral-300" />
               </Dialog.Close>
             </Dialog.Content>
@@ -58,17 +55,14 @@ export default function Image(props: ImageProps) {
       {/* Author Information */}
       <span className="absolute bottom-4 left-4 flex gap-4">
         <div className="flex flex-col items-start gap-2">
-          <span className="font-semibold overflow-hidden line-clamp-1 text-ellipsis w-2/3">
-            {props.caption}
-          </span>
+          <span className="font-semibold overflow-hidden line-clamp-1 text-ellipsis w-2/3">{props.caption}</span>
 
           <span className="text-xs font-semibold flex gap-1.5">
             <img src={props.author.avatar} className="w-4 h-4 rounded-full" />
             <span>{props.author.name}</span>
 
             <span className="before:content-[''] before:bg-current before:w-1 before:h-1 before:block before:rounded-full flex items-center gap-1">
-              {props.views.toLocaleString('en-US', { notation: 'compact' })}{' '}
-              Views
+              {props.views.toLocaleString('en-US', { notation: 'compact' })} Views
             </span>
           </span>
         </div>
@@ -76,9 +70,7 @@ export default function Image(props: ImageProps) {
 
       {/* Heart Icon */}
       <div className="absolute bottom-8 right-4 flex items-center gap-2">
-        <span>
-          {props.likes.toLocaleString('en-US', { notation: 'compact' })}
-        </span>
+        <span>{props.likes.toLocaleString('en-US', { notation: 'compact' })}</span>
 
         <button
           className={`hover:bg-neutral-600 hover:bg-opacity-30 rounded-full p-2 ${
