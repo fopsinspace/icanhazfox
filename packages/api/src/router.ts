508c46a2da6015fee7a8be4c92d7996c32bd3296
ivy <ivy@not-ivy.dev>
2023-11-11 20:30:22 -0800

    fix(api): add signup procedure to trpc router

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,9 +1,11 @@
 import { router } from './trpc';
 
 import hello from './procedures/hello';
+import signUp from './procedures/signup';
 
 export const appRouter = router({
   hello,
+  signUp
 });
 
-export type Approuter = typeof appRouter;
+export type AppRouter = typeof appRouter;
