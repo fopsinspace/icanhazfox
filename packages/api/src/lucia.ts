c4353603a96d09a63f23f3dde5b0de69ace297a7
ivy <ivy@not-ivy.dev>
2023-11-11 22:04:11 -0800

    fix(api): getUserAttributes in lucia

--- packages/api/src/lucia.ts
+++ packages/api/src/lucia.ts
@@ -9,10 +9,10 @@ export const initializeLucia = (db: D1Database, env: 'DEV' | 'PROD') =>
       session: 'user_session',
     }),
     env,
-    // getUserAttributes: (data) => ({
-    //   username: data?.username,
-    //   avatar: data?.avatar,
-    // }),
+    getUserAttributes: (data) => ({
+      username: data?.username,
+      avatar: data?.avatar,
+    }),
     experimental: {
       debugMode: env === 'DEV',
     }
