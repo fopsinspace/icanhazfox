eef34325487498fe367d195b8abc6b811f0353fe
ivy <ivy@not-ivy.dev>
2023-11-11 22:05:03 -0800

    chore(api): oops

--- packages/api/src/procedures/hello.ts
+++ packages/api/src/procedures/hello.ts
@@ -3,7 +3,6 @@ import { procedure } from '../trpc';
 export default procedure.query(({ ctx }) => {
   return new Promise((resolve, reject) => {
     ctx.resHeaders.set('amog', Math.random() > 0.5 ? 'impostor' : 'crewmate');
-    ctx.resHeaders.set('Set-Cookie', 'kms=yes')
     setTimeout(Math.random() > 0.5 ? resolve : reject, 800);
   });
 });
